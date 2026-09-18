53feaa31df53ca4e1fb67065594feb18b1de39f3
Aidan Grier <106179841@student.swin.edu.au>
2026-09-18 15:59:54 +1000

    added editor project and configured program to copy assets to bin dir

---
+++ Editor/Editor.cs
@@ -0,0 +1,64 @@
+using SplashKitSDK;
+using CustomProgram;
+using System.ComponentModel;
+namespace Editor
+{
+    class Editor
+    {
+        private String _windowName;
+        private Window _window;
+        private String _currentLevel = "./levels/2.lvl";
+        public Editor() : this("CustomProgram") { }
+        public Editor(String windowTitle)
+        {
+            _windowName = windowTitle;
+            _window = SplashKit.OpenWindow(_windowName, 800, 600);
+        }
+        public void Run()
+        {
+            Bitmap background = SplashKit.LoadBitmap("background", "./assets/background.png");
+
+            Level myLevel = new Level(_currentLevel);
+            Player p1 = new Player(myLevel);
+            p1.X = 3;
+            p1.Y = 4;
+
+            while (!SplashKit.WindowCloseRequested(_windowName))
+            {
+                SplashKit.ProcessEvents();
+                // player input
+                if (SplashKit.KeyDown(KeyCode.WKey))
+                {
+                    p1.Move(MoveDirection.Up);
+                }
+                else if (SplashKit.KeyDown(KeyCode.SKey))
+                {
+                    p1.Move(MoveDirection.Down);
+                }
+                else if (SplashKit.KeyDown(KeyCode.AKey))
+                {
+                    p1.Move(MoveDirection.Left);
+                }
+                else if (SplashKit.KeyDown(KeyCode.DKey))
+                {
+                    p1.Move(MoveDirection.Right);
+                }
+                // debug keybinds
+                if (SplashKit.KeyTyped(KeyCode.RKey))
+                {
+                    myLevel.Load(_currentLevel);
+                }
+                p1.Update();
+                myLevel.Update();
+                // SplashKit.ClearScreen(Color.Black);
+                background.Draw(0, 0);
+                myLevel.Draw();
+                p1.Draw();
+                myLevel.DrawObjects();
+                // draw scaled buffer
+                SplashKit.RefreshScreen(60);
+            }
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
---
+++ Editor/Program.cs
@@ -0,0 +1,12 @@
+﻿using SplashKitSDK;
+namespace Editor
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Editor editor = new Editor();
+            editor.Run();
+        }
+    }
+}
